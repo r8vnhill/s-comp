f050be816a3e3fc4d90724cd9d4ed90b46237882
R8V <ignacio.slater@ug.uchile.cl>
2024-01-05 21:21:55 -0300

    🔧✨📝 Update interpret function to use environment mapping
    The interpret function now takes an additional parameter, an environment mapping, to resolve symbols. The update also includes handling for ast.Var type in the function's pattern match. This enhancement enables interpretation of symbol-based expressions.

--- src/main/scala/Interpret.scala
+++ src/main/scala/Interpret.scala
@@ -2,7 +2,9 @@ package cl.ravenhill.scum
 
 import scala.util.{Success, Try}
 
-
-def interpret[A](expression: ast.Expression[A]): Try[Int] = expression match {
-  case ast.Num(value) => Success(value)
+def interpret[A](expression: ast.Expression[A], environment: Map[String, Int]): Try[Int] = {
+  expression match {
+    case ast.Num(value) => Success(value)
+    case ast.Var(sym) => Try(environment(sym))
+  }
 }
