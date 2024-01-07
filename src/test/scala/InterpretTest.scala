a7d503ad7bc700c7f2908d1d434061ca18620c49
R8V <ignacio.slater@ug.uchile.cl>
2024-01-07 11:39:59 -0300

    🔨✅🔒 Refactor InterpretTest and expand test cases
    The InterpretTest class has been refactored with the inclusion of AstGenerators. Import statements have been streamlined and additional test cases have been introduced to improve testing coverage. The 'testCases' variable was also made private for better encapsulation.

--- src/test/scala/InterpretTest.scala
+++ src/test/scala/InterpretTest.scala
@@ -1,13 +1,15 @@
 package cl.ravenhill.scum
 
-import cl.ravenhill.scum.ast.{IdLiteral, NumericLiteral}
+import ast.*
+
+import cl.ravenhill.scum.generators.AstGenerators
 
 import scala.util.Success
 
-class InterpretTest extends AbstractScumTest {
-  val testCases = Table(
+class InterpretTest extends AbstractScumTest with AstGenerators {
+  private val testCases = Table(
     ("ast", "expected"),
-    // Numeric literals
+
     (NumericLiteral(1), Success(1L)),
     (NumericLiteral(420), Success(420L)),
     (NumericLiteral(1234567890123456789L), Success(1234567890123456789L)),
@@ -17,13 +19,29 @@ class InterpretTest extends AbstractScumTest {
     (NumericLiteral(0), Success(0L)),
     (NumericLiteral(Long.MinValue), Success(Long.MinValue)),
     (NumericLiteral(Long.MaxValue), Success(Long.MaxValue)),
-    // Identifiers
+
+    (IdLiteral("x"), Success(1L)),
+    (IdLiteral("y"), Success(2L)),
+    (IdLiteral("z"), Success(3L)),
+
+    (Increment(IdLiteral("x")), Success(2L)),
+    (Increment(Increment(NumericLiteral(1))), Success(3L)),
+
+    (Decrement(IdLiteral("x")), Success(0L)),
+    (Decrement(Decrement(NumericLiteral(1))), Success(-1L)),
   )
 
   "Interpreter can interpret syntax tree" in {
     forAll(testCases) { (ast, expected) =>
       val result = interpret(ast, Map("x" -> 1L, "y" -> 2L, "z" -> 3L))
       result should be(expected)
+    }
+  }
+
+  "Decrement and increment are symmetric properties" in {
+    forAll(generateExpression()) { ast =>
+     interpret(ast, Map("ZERO" -> 0)) should be(interpret(Increment(Decrement(ast)), Map("ZERO" -> 0)))
+     interpret(ast, Map("ZERO" -> 0)) should be(interpret(Decrement(Increment(ast)), Map("ZERO" -> 0)))
     }
   }
 }
